8e7c3d983d862f3940047a94c0863c8a0d8b494d
Lethabo Motloung <106984562+LethaboMot@users.noreply.github.com>
2023-05-08 14:41:31 +0200

    edit

---
+++ dist/index.js
@@ -0,0 +1,46 @@
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
+
+addToList = () => {
+    if(inputBox.value === ''){
+        
+    } else {
+        let li = document.createElement('li'); // in the line it is making one HTML element with the tag name 'li'
+        li.innerHTML = inputBox.value; //making as whenever someone text in the input it appears there
+        listContainer.appendChild(li); // where the content of the input should displayed
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    saveData();
+}
+
+listContainer.addEventListener('click', function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked'); //making the line through the middle when you the user clicks on the to-do list
+        saveData();
+    } else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false)
+// localStorage
+
+saveData = () => {
+    localStorage.setItem('data',listContainer.innerHTML);
+}
+
+showList = () => {
+  listContainer.innerHTML = localStorage.getItem('data')
+}
+showList();
+
+removeList = () => {
+  // Select the to-do list element
+  var list = document.getElementById('list-container');
+  // Remove all the child elements of the to-do list element
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
-const path = require('path')
-module.exports = { 
-  mode: 'development',
-	bundle: path.resolve(__dirname, 'src/index.js'),
-	output: {
-		path: path.resolve(__driname, 'dist'),
-		filename: '[name].js'
-	},
-}
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+ module.exports = {
+   entry: {
+     index: './src/index.js',
+     print: './src/print.js',
+   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+   output: {
+     filename: '[name].bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+   },
+ };
