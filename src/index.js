d99e037e6fcc6bd0f2d61c4cc195a0a4b4d012a9
Lethabo Motloung <106984562+LethaboMot@users.noreply.github.com>
2023-05-11 12:49:53 +0200

    Fix eslint

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ listContainer.addEventListener('click', (e) => {
   if (e.target.tagName === 'LI') {
     e.target.classList.toggle('checked');
   } else if (e.target.tagName === 'SPAN') {
-    e.target.parentElement.remove();  
+    e.target.parentElement.remove();
   }
 }, false);
 
