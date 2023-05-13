ab7dae8583940eb4f239811847d37746f87adba8
Lethabo Motloung <106984562+LethaboMot@users.noreply.github.com>
2023-05-13 23:18:01 +0200

    Added the checkbox

--- src/modules/main.js
+++ src/modules/main.js
@@ -2,25 +2,49 @@ const inputBox = document.getElementById('input-box');
 const listContainer = document.getElementById('list-container');
 const STORAGE_KEY = 'list_data';
 
-// localStorage
 function saveData() {
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(listContainer.innerHTML));
+  const tasks = [];
+  const listItems = listContainer.getElementsByTagName('li');
+  for (let i = 0; i < listItems.length; i++) {
+    const task = {
+      taskName: listItems[i].textContent,
+      completed: listItems[i].querySelector('input[type=checkbox]').checked
+    };
+    tasks.push(task);
+  }
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks));
 }
 
 function showList() {
   const savedData = localStorage.getItem(STORAGE_KEY);
   if (savedData) {
-    listContainer.innerHTML = JSON.parse(savedData);
+    const tasks = JSON.parse(savedData);
+    for (let i = 0; i < tasks.length; i++) {
+      const li = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.checked = tasks[i].completed;
+      li.appendChild(checkbox);
+      li.appendChild(document.createTextNode(tasks[i].taskName));
+      listContainer.appendChild(li);
+      const span = document.createElement('span');
+      span.innerHTML = '\u00d7';
+      li.appendChild(span);
+    }
   }
 }
+
 showList();
 
 const addToList = () => {
   if (inputBox.value === '') {
-    alert('');
+    alert('Please enter a task');
   } else {
     const li = document.createElement('li');
-    li.innerHTML = inputBox.value;
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    li.appendChild(checkbox);
+    li.appendChild(document.createTextNode(inputBox.value));
     listContainer.appendChild(li);
     const span = document.createElement('span');
     span.innerHTML = '\u00d7';
@@ -31,25 +55,25 @@ const addToList = () => {
 };
 
 function removeList() {
-  // Select the to-do list element
-  const list = document.getElementById('list-container');
-  // Remove all the child elements of the to-do list element
-  while (list.firstChild) {
-    list.removeChild(list.firstChild);
+  while (listContainer.firstChild) {
+    listContainer.removeChild(listContainer.firstChild);
   }
-  saveData();
+  localStorage.removeItem(STORAGE_KEY);
 }
 
 function editList(e) {
   if (e.target.tagName === 'LI') {
     const input = document.createElement('input');
     input.type = 'text';
-    input.value = e.target.textContent;
+    input.value = e.target.textContent.trim();
     e.target.replaceWith(input);
     input.addEventListener('keypress', (e) => {
       if (e.key === 'Enter') {
         const li = document.createElement('li');
-        li.textContent = e.target.value;
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        li.appendChild(checkbox);
+        li.appendChild(document.createTextNode(input.value.trim()));
         input.replaceWith(li);
         saveData();
       }
@@ -58,7 +82,9 @@ function editList(e) {
 }
 
 listContainer.addEventListener('click', (e) => {
-  if (e.target.tagName === 'LI') {
+  if (e.target.tagName === 'INPUT') {
+    saveData();
+  } else if (e.target.tagName === 'LI') {
     e.target.classList.toggle('checked');
     saveData();
   } else if (e.target.tagName === 'SPAN') {
@@ -67,13 +93,7 @@ listContainer.addEventListener('click', (e) => {
   }
 });
 
-function activateEdit() {
-  const li = document.querySelector('li');
-  li.addEventListener('dblclick', editList);
-}
 listContainer.addEventListener('dblclick', editList);
 
 document.querySelector('.add-btn').addEventListener('click', addToList);
 document.querySelector('.removeButton').addEventListener('click', removeList);
-// double click the items on the list to edit
-document.querySelector('li').addEventListener('click', activateEdit);
